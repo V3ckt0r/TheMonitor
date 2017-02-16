b1e2cd4877ca5a5f449989f8095eb07772ed3b65
V3ckt0r <burhan_abdi@hotmail.co.uk>
2017-02-16 14:13:38 +0000

    Added code test coverage report

--- Themonitor/test/themonitor_test.py
+++ Themonitor/test/themonitor_test.py
@@ -1,6 +1,11 @@
 import unittest
 import os
 import sys
+from coverage import coverage
+
+# initialise coverage
+cov = coverage(branch=True, omit=['flask/*', 'themonitor_test.py', '/Library/*'])
+cov.start()
 
 # Add source home into python path and import themonitor.py
 mypath = os.path.abspath('..')
@@ -23,5 +28,21 @@ class ThemonitorTestCase(unittest.TestCase):
         rv = self.app.get('/lookup')
         assert "stage.tools.bbc.co.uk" in rv.data
 
+    def test_api_site(self):
+        rv = self.app.get('/api/v1.0/site')
+        assert "Active_site" in rv.data
+
+
 if __name__ == '__main__':
-    unittest.main()
+    try:
+        unittest.main()
+    except:
+        pass
+    cov.stop()
+    cov.save()
+    print("\n\nCoverage Report:\n")
+    cov.report()
+    basedir = os.path.join(os.path.dirname(__file__), 'Themonitor')
+    print("HTML version: " + os.path.join(basedir, "tmp/coverage/index.html"))
+    cov.html_report(directory='tmp/coverage')
+    #cov.erase()
